d9730f4ed0a4a2d60335641e0af4c24480f1656b
Theophilus Nenhanga <65165568+TeeEnnEnn@users.noreply.github.com>
2023-09-23 05:42:24 -0600

    Links from the buttons
    * Room names are now links

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/socket.io': {
-        target: 'http://127.0.0.1:3001',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
       },
       '/api': {
-        target: 'http://127.0.0.1:3001',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
       }
     }
