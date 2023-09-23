ff15b6bc8f7f26fddb25fae5d5d73b86acd31c57
Theophilus Nenhanga <65165568+TeeEnnEnn@users.noreply.github.com>
2023-09-22 20:59:45 -0600

    Added Socket
    Added basic socket intergration

--- wanLan/__init__.py
+++ wanLan/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
+from flask_socketio import SocketIO
+
 
 def create_app():
     app: Flask = Flask(__name__)
     app.config["SECRET_KEY"] = "WAN"
 
-    return app
+    socketio = SocketIO(app)
+
+    return app, socketio
+
 
 def register_blueprints(app):
-
     from wanLan.views import main
-
     app.register_blueprint(main, url_prefix="/")
--- wanLan/views.py
+++ wanLan/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 main = Blueprint("main", __name__)
 
+
 @main.route("/", methods=["GET", "POST"])
 def home():
     return render_template()
