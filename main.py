ce78fb35ffda4eb6704ef04d38212ba62d35b86d
chukwunyemcyril <100722653+chukwunyemcyril@users.noreply.github.com>
2023-09-22 23:07:32 -0600

    Added Message Handling
    Added Message Handling

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+import socketio
+
 from wan import create_app
+from flask_socketio import send, emit
+
+
+@socketio.on('message')
+def handle_message(message):
+    send(message)
+
+
+@socketio.on('json')
+def handle_json(json):
+    send(json, json=True)
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    emit('my resposne', json)
+
 
 app, socketio = create_app()
 
