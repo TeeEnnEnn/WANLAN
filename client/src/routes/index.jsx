a5d9542643dcbea1385860479d74a64aa17d117d
Theophilus Nenhanga <65165568+TeeEnnEnn@users.noreply.github.com>
2023-09-23 04:42:16 -0600

    More Styles
    Added more styles

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,9 +1,11 @@
 import {UsernameForm} from "../components/usernameForm.jsx";
+import {HostForm} from "../components/HostForm.jsx";
 
 export function Index() {
     return (
         <div>
             <UsernameForm />
+            <HostForm />
         </div>
     )
 }
