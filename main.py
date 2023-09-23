5e96790f16498384a120ed5a8e5095fafd5b5775
Theophilus Nenhanga <65165568+TeeEnnEnn@users.noreply.github.com>
2023-09-22 22:06:08 -0600

    Socket initialization
    * initialized socket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from wan import create_app
 
-app = create_app()
+app, socketio = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
--- wan/__init__.py
+++ wan/__init__.py
@@ -1,13 +1,14 @@
+from flask_socketio import SocketIO
+
 from flask import Flask
 
 
 def create_app():
     app: Flask = Flask(__name__)
     app.config["SECRET_KEY"] = "wan"
+    socketio = SocketIO(app)
 
     from wan.main.views import main
     app.register_blueprint(main, url_prefix="/")
 
-    return app
-
-
+    return app, socketio
