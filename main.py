f3a262c03dd5091e2fcda1b73308fb2e133f22a0
jrsswish <quodet.johnrey@gmail.com>
2023-09-22 22:20:56 -0600

    join and leave room socketio

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from wan import create_app
+from flask_socketio import join_room, leave_room, send
+
 
 app, socketio = create_app()
 
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room', to=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
+
 if __name__ == "__main__":
     socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+
+
